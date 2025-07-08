214366e424ba049d80f8315d499991b8884afd41
ShenranTomWang <wsr.20021128@gmail.com>
2025-07-08 18:52:40 +0000

    debug

--- inference.sh
+++ inference.sh
@@ -1,15 +1,13 @@
 python inference.py
     --cache_dir $HF_HOME
+    --checkpoint /home/shw002/u/seed-vc/models/DiT_uvit_tat_xlsr_ema.pth
+    --config /home/shw002/u/seed-vc/models/config_dit_mel_seed_uvit_xlsr_tiny.yml
     singular
         --source ../StyleTTS2/out/average/4a45e4b0-b95f-4590-a0ed-9949f06fdfbb/3f6c935bce2432fffbbcddfbf14f0e33f25097d14ff37046ef076fea7e15885d.wav
-        --output out
-        --checkpoint /home/shw002/u/seed-vc/models/DiT_uvit_tat_xlsr_ema.pth
-        --config /home/shw002/u/seed-vc/models/config_dit_mel_seed_uvit_xlsr_tiny.yml
 
 python inference.py
     --cache_dir $HF_HOME
+    --checkpoint /home/shw002/u/seed-vc/models/DiT_uvit_tat_xlsr_ema.pth
+    --config /home/shw002/u/seed-vc/models/config_dit_mel_seed_uvit_xlsr_tiny.yml
     directory
         --dir ../StyleTTS2/out/average
-        --output out
-        --checkpoint /home/shw002/u/seed-vc/models/DiT_uvit_tat_xlsr_ema.pth
-        --config /home/shw002/u/seed-vc/models/config_dit_mel_seed_uvit_xlsr_tiny.yml
