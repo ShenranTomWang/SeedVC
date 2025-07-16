ee52ac71c7935dccefd7e370d62046c95c1577d1
ShenranTomWang <wsr.20021128@gmail.com>
2025-07-16 23:52:35 +0000

    iterate through speakers

--- inference_v2.sh
+++ inference_v2.sh
@@ -14,70 +14,73 @@ conda activate VC
 cd SeedVC
 export HF_HOME=/home/shw002/u/tmp
 export ROOT=/home/shw002/StyleTTS2/out
-export SPEAKER_ID1=118
-export SPEAKER_ID2=47824
-export SPEAKER_UTTERANCE1=000003
-export SPEAKER_UTTERANCE2=000001
-export WAV_NAME="${SPEAKER_ID1}_${SPEAKER_ID2}_${SPEAKER_UTTERANCE1}_${SPEAKER_UTTERANCE2}"
-export TARGET=/home/shw002/u/data/LibriTTS_R/train-clean-100/${SPEAKER_ID1}/${SPEAKER_ID2}/${WAV_NAME}.wav
 
-for parent_dir in \
-    baseline \
-    average \
-    gender_average \
-    gender_average_swapped \
-    noise \
-    steer_f2m \
-    steer_m2f \
-    custom_libritts_female \
-    custom_libritts_male \
-    s_a_average \
-    s_a_gender_average \
-    s_a_gender_average_swapped \
-    s_a_noise \
-    s_a_steer_m2f \
-    s_a_steer_f2m \
-    s_a_custom_libritts_female \
-    s_a_custom_libritts_male \
-    pca/top_32_average \
-    pca/top_32_noise \
-    pca/top_32_steer_f2m \
-    pca/top_32_steer_m2f \
-    pca/top_32_gender_average \
-    pca/top_32_gender_average_swapped \
-    pca/top_32_custom_libritts_female \
-    pca/top_32_custom_libritts_male \
-    gender_logistic_regression/top_32_average \
-    gender_logistic_regression/top_32_noise \
-    gender_logistic_regression/top_32_steer_f2m \
-    gender_logistic_regression/top_32_steer_m2f \
-    gender_logistic_regression/top_32_gender_average \
-    gender_logistic_regression/top_32_gender_average_swapped \
-    gender_logistic_regression/top_32_custom_libritts_female \
-    gender_logistic_regression/top_32_custom_libritts_male \
-    pca/s_a_top_32_average \
-    pca/s_a_top_32_noise \
-    pca/s_a_top_32_steer_f2m \
-    pca/s_a_top_32_steer_m2f \
-    pca/s_a_top_32_gender_average \
-    pca/s_a_top_32_gender_average_swapped \
-    pca/s_a_top_32_custom_libritts_female \
-    pca/s_a_top_32_custom_libritts_male \
-    gender_logistic_regression/s_a_top_32_average \
-    gender_logistic_regression/s_a_top_32_noise \
-    gender_logistic_regression/s_a_top_32_steer_f2m \
-    gender_logistic_regression/s_a_top_32_steer_m2f \
-    gender_logistic_regression/s_a_top_32_gender_average \
-    gender_logistic_regression/s_a_top_32_gender_average_swapped \
-    gender_logistic_regression/s_a_top_32_custom_libritts_female \
-    gender_logistic_regression/s_a_top_32_custom_libritts_male
+for wav_name in \
+    27_123349_000001_000000 \
+    40_121026_000008_000000 \
+    118_47824_000003_000001
 do
-    python inference_v2.py \
-        --ar-checkpoint-path /home/shw002/u/SeedVC/models/v2/ar_base.pth \
-        --cfm-checkpoint-path /home/shw002/u/SeedVC/models/v2/cfm_small.pth \
-        --target $TARGET \
-        --output out/v2/$WAV_NAME \
-        directory \
-            --dir $ROOT/$parent_dir \
-            --root $ROOT
+    IFS="_" read -ra wav_name_split <<< "$wav_name"
+    target="/home/shw002/u/data/LibriTTS_R/train-clean-100/${wav_name_split[0]}/${wav_name_split[1]}/${wav_name_split[2]}_${wav_name_split[3]}.wav"
+    for parent_dir in \
+        baseline \
+        average \
+        gender_average \
+        gender_average_swapped \
+        noise \
+        steer_f2m \
+        steer_m2f \
+        custom_libritts_female \
+        custom_libritts_male \
+        s_a_average \
+        s_a_gender_average \
+        s_a_gender_average_swapped \
+        s_a_noise \
+        s_a_steer_m2f \
+        s_a_steer_f2m \
+        s_a_custom_libritts_female \
+        s_a_custom_libritts_male \
+        pca/top_32_average \
+        pca/top_32_noise \
+        pca/top_32_steer_f2m \
+        pca/top_32_steer_m2f \
+        pca/top_32_gender_average \
+        pca/top_32_gender_average_swapped \
+        pca/top_32_custom_libritts_female \
+        pca/top_32_custom_libritts_male \
+        gender_logistic_regression/top_32_average \
+        gender_logistic_regression/top_32_noise \
+        gender_logistic_regression/top_32_steer_f2m \
+        gender_logistic_regression/top_32_steer_m2f \
+        gender_logistic_regression/top_32_gender_average \
+        gender_logistic_regression/top_32_gender_average_swapped \
+        gender_logistic_regression/top_32_custom_libritts_female \
+        gender_logistic_regression/top_32_custom_libritts_male \
+        pca/s_a_top_32_average \
+        pca/s_a_top_32_noise \
+        pca/s_a_top_32_steer_f2m \
+        pca/s_a_top_32_steer_m2f \
+        pca/s_a_top_32_gender_average \
+        pca/s_a_top_32_gender_average_swapped \
+        pca/s_a_top_32_custom_libritts_female \
+        pca/s_a_top_32_custom_libritts_male \
+        gender_logistic_regression/s_a_top_32_average \
+        gender_logistic_regression/s_a_top_32_noise \
+        gender_logistic_regression/s_a_top_32_steer_f2m \
+        gender_logistic_regression/s_a_top_32_steer_m2f \
+        gender_logistic_regression/s_a_top_32_gender_average \
+        gender_logistic_regression/s_a_top_32_gender_average_swapped \
+        gender_logistic_regression/s_a_top_32_custom_libritts_female \
+        gender_logistic_regression/s_a_top_32_custom_libritts_male
+    do
+        python inference_v2.py \
+            --ar-checkpoint-path /home/shw002/u/SeedVC/models/v2/ar_base.pth \
+            --cfm-checkpoint-path /home/shw002/u/SeedVC/models/v2/cfm_small.pth \
+            --target $target \
+            --output out/v2/$wav_ame \
+            --anonymization-only true \
+            directory \
+                --dir $ROOT/anonymized/$parent_dir \
+                --root $ROOT
+    done
 done
